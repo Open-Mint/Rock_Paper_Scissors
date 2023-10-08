59273ab8fe93d87e58f790a9180116063040e5b3
Open Mint <mintopen33@gmail.com>
2023-10-08 23:14:55 +0200

    add choices

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
-
 int main()
 {
-    std::cout << "Hello, world\n";
+    std::cout << "\"Rock Paper Scissors\"\n_______________________________\n";
+    std::cout << "Choose a number:\nRock: 0  Paper: 1  Scissors: 2\n";
+    
+    int choice = 0;
+    std::cin >> choice;
+
     return 0;
 }
