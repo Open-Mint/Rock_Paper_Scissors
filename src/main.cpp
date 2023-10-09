80b164aeee94dc56bbb6bcefb5ca7aa0f0ec238c
Open Mint <mintopen33@gmail.com>
2023-10-09 10:44:48 +0200

    add Player class

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <random>
 
+class Player
+{
+public:
+    int choice;
+private:
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> dist;
+public:
+    Player()
+    : gen(rd()), dist(0, 2)
+    {
+        choice = dist(gen);
+    }
+};
 int main()
 {
     std::cout << "\"Rock Paper Scissors\"\n_______________________________\n";
     std::cout << "Choose a number:\nRock: 0  Paper: 1  Scissors: 2\n";
     
-    int choice = 0;
-    std::cin >> choice;
+    Player computer;
 
     return 0;
 }
