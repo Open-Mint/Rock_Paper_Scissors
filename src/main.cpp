cb32f340ee661c1522beef2115d6fbb2fe350483
Open Mint <mintopen33@gmail.com>
2023-10-09 11:40:19 +0200

    add playing logic

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,32 @@ int main()
 {
     std::cout << "\"Rock Paper Scissors\"\n_______________________________\n";
     std::cout << "Choose a number:\nRock: 0  Paper: 1  Scissors: 2\n";
+
+    int choice = 0;
+    std::cin >> choice;    
+    std::cout << "_______________________________\n";
     
     Player computer;
+
+    std::cout << "You chose : " << choice << '\n';
+    std::cout << "Computer chose: " << computer.choice << '\n';
+
+    if(computer.choice == choice)
+    {
+        std::cout << "Draw. Try again\n";
+    }
+    else
+    if((choice == 0 && computer.choice == 1) || (choice == 1 && computer.choice == 2)
+       || (choice == 2 && computer.choice == 0))
+    {
+        std::cout << "You lost\n";
+    }
+    else
+    {
+        std::cout << "You won\n";
+    }
+
+    std::cout << "_______________________________\n";
 
     return 0;
 }
